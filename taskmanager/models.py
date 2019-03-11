959509e0770e9539e249b3df26bfbafdc34d64ff
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-11 11:05:40 +0530

    updated model

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Team(models.Model):
     class Meta:
@@ -9,36 +11,37 @@ class Team(models.Model):
     title = models.CharField(_("title"), max_length=200)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='team_created', verbose_name=_('created by'),
                                    on_delete=models.SET_NULL, null=True)
-    users = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='team_users', verbose_name=_('team_users'),
-                                   on_delete=models.SET_NULL, null=True, blank=True)
+    users = models.ManyToManyField(User)
+    
     def __str__(self):
-        return "[%s] %s" % (self.id, self.title)
-
+        return self.title
 
 class Task(models.Model):
     class Meta:
         verbose_name = _("Task")
         verbose_name_plural = _("Tasks")
-
+    
     STATUSES = (
         ('to-do', _('To Do')),
+        ('planned', _('Planned')),
         ('in_progress', _('In Progress')),
-        ('blocked', _('Blocked')),
         ('done', _('Done')),
         ('dismissed', _('Dismissed'))
     )
 
-    title = models.CharField(_("title"), max_length=200)
-    description = models.TextField(_("description"), max_length=2000, null=True, blank=True)
-    resolution = models.TextField(_("resolution"), max_length=2000, null=True, blank=True)
-    deadline = models.DateField(_("deadline"), null=True, blank=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks_assigned', verbose_name=_('assigned to'),
-                                   on_delete=models.SET_NULL, null=True, blank=True)
-    state = models.CharField(_("state"), max_length=20, choices=STATUSES, default='to-do')
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='tasks_created', verbose_name=_('created by'),
-                                   on_delete=models.SET_NULL, null=True)
+    title = models.CharField(_("title"), max_length=50)
+    description = models.TextField(max_length=1000)
+    team = models.ForeignKey(Team,on_delete=models.PROTECT, null =True, blank = True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='task_creator', verbose_name=_('task_creator'),
+                                   on_delete=models.PROTECT, null=False)
+    # assignee = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='task_assigne', verbose_name=_('task_assigne'),
+    #                               on_delete=models.SET_NULL, null=True,blank=True)
+    assignee = models.ManyToManyField(User,related_name='task_assigne', verbose_name=_('task_assigne'), null=True,blank=True)
+    status = models.CharField(_("status"), max_length=20, choices=STATUSES,default='to-do')
     created_at = models.DateTimeField(_("created at"), auto_now_add=True, editable=False)
     last_modified = models.DateTimeField(_("last modified"), auto_now=True, editable=False)
-
+    
     def __str__(self):
-        return "[%s] %s" % (self.id, self.title)
+        return self.title
+    def get_assignee(self):
+        return ",".join([str(a) for a in self.assignee.all()])
