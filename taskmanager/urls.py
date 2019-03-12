06025f4c766156301dad19474555b5fb0ae2274f
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-12 11:13:57 +0530

    updated urls for team and task

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
-from django.conf.urls import url,include
 from taskmanager import views
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-
+#from taskapp.views import AutocompleteJsonView
 urlpatterns = [
     path('', views.home,name='home'),
-]
+
+    # Team urls
+    url(r'^team/create/$', views.teamreg,name='teamreg'),
+    url(r'^team/view/(?P<string>.+)/$', views.teamview,name='teamview'),
+    url(r'^team/edit/(?P<string>.+)/$', views.teamedit,name='teamedit'),
+    
+    # Task urls
+    url(r'^task/create/$', views.taskreg,name='taskreg'),
+    url(r'^task/view/(?P<string>.+)/$', views.taskview,name='taskview'),
+    url(r'^task/edit/(?P<string>.+)/$', views.taskedit,name='taskedit'),
+    url(r'^task/delete/(?P<string>.+)/$', views.taskdelete,name='taskdelete'),
+  
