aacc59f941258b2d32d0eb1e109e4bf25e914445
Shailesh Kumar Sahu <shellkoresahu@gmail.com>
2019-03-15 11:34:11 +0530

    added comment in URLS

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,4 +1,6 @@
-from taskmanager import views
+from django.urls import path
+from django.conf.urls import url,include
+from taskapp import views
 from django.views.generic.base import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
@@ -16,4 +18,14 @@ urlpatterns = [
     url(r'^task/view/(?P<string>.+)/$', views.taskview,name='taskview'),
     url(r'^task/edit/(?P<string>.+)/$', views.taskedit,name='taskedit'),
     url(r'^task/delete/(?P<string>.+)/$', views.taskdelete,name='taskdelete'),
-  
+    
+    # Comments urls
+    url(r'^comment/edit/(?P<id>[\w\-]+)/$', views.commentedit,name='commentedit'),
+    url(r'^comment/delete/(?P<id>[\w\-]+)/$', views.commentdel,name='commentdel'),
+    
+    # Ajax requests urls
+    url(r'^ajax/users/$', views.users_list, name='users_list'),
+    url(r'^ajax/team/users/$', views.team_users_list, name='team_users_list'),
+    url(r'^ajax/teams/$', views.teams_list, name='teams_list'),
+    
+]
